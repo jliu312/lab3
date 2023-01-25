c631dce873e36de687f5964cc594e3cab59a6c67
jliu312 <85335934+jliu312@users.noreply.github.com>
2023-01-25 15:18:22 -0800

    fixed ReverseInPlace() and testReversed()

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,21 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 1, 2, 3, 4 };
+    int[] input2 = {1, 2, 3};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 4, 3, 2, 1 }, input1);
+    assertArrayEquals(new int[]{3, 2, 1}, input2);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = { 1, 2, 3 };
+    assertArrayEquals(new int[]{ 3, 2, 1 }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {1, 2, 3, 4};
+    assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input2));
   }
 }
